34126e7bb2d5a9d370c29a9b6ca36fa8465fdd58
Josua Hämmerle <josua.haemmerle@ricardo.ch>
2020-04-14 13:54:44 +0200

    trying something with search

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -12,13 +12,14 @@ export default function MainPage() {
     useEffect(() => {
         fetchData()
         console.log('USEEFFECT Page: ', page)
+        console.log('Characters: ', characters)
     }, [page])
     
     const fetchData = async () => {
         const response = await fetch(`https://rickandmortyapi.com/api/character/?name=${searchTerm}&page=${page}`)
         const data = await response.json()
         if(data.results) {
-            setCharacters(characters => [...characters, ...data.results])
+            setCharacters([...characters, ...data.results])
         }
     }
 
@@ -26,19 +27,17 @@ export default function MainPage() {
         setPage(page => page + 1)
     }
 
-    const helperFunction = () => {
-        setPage(1)
-        searchFunction()
-    }
-
     const searchFunction = async () => {
         // This is actually not working because it only LATER sets the page to 1 again and the fetch is done before that
         // setPage(1) 
 
-        const response = await fetch(`https://rickandmortyapi.com/api/character/?name=${searchTerm}&page=${page}`)
+        const response = await fetch(`https://rickandmortyapi.com/api/character/?name=${searchTerm}&page=1`)
         const data = await response.json()
         if(data.results) {
             setCharacters([...data.results])
+        }
+        else {
+            setCharacters([])
         }
     }
     
@@ -47,9 +46,6 @@ export default function MainPage() {
         setSearchTerm(value)
     }
     
-    // const leaveInput = () => {
-    //     setPage(1)
-    // }
 
 
     return(
@@ -58,7 +54,7 @@ export default function MainPage() {
 
             <div className='searchBar'>
                 <input type="search" name='searchField' value={searchTerm} placeholder='Type a character name...' onChange={handleChange} />
-                <button className='btn search-btn' onClick={helperFunction} >Search</button>
+                <button className='btn search-btn' onClick={searchFunction} >Search</button>
             </div>
 
             <div className='main-container'>
