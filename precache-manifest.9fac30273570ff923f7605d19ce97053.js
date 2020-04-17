f170e1ecb5e9beba2617aa66be517eca376c306f
Josua Hämmerle <josua.haemmerle@ricardo.ch>
2020-04-17 12:04:31 +0200

    Updates

---
+++ precache-manifest.9fac30273570ff923f7605d19ce97053.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07cab3e1f4960fc4f76736e63895d230",
+    "url": "/rick-and-morty/index.html"
+  },
+  {
+    "revision": "18d4f23acd00d6386edc",
+    "url": "/rick-and-morty/static/css/main.56f4b543.chunk.css"
+  },
+  {
+    "revision": "6e5df95586bf41d35431",
+    "url": "/rick-and-morty/static/js/2.8145dde7.chunk.js"
+  },
+  {
+    "revision": "c3f4a91b08326551c3d2c23a3296dfa8",
+    "url": "/rick-and-morty/static/js/2.8145dde7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18d4f23acd00d6386edc",
+    "url": "/rick-and-morty/static/js/main.136eda23.chunk.js"
+  },
+  {
+    "revision": "c1b7ac5fbc2c52e1e347",
+    "url": "/rick-and-morty/static/js/runtime-main.9ea3f8cd.js"
+  },
+  {
+    "revision": "e05c4cf52c9901c033432c20f3d0b8d4",
+    "url": "/rick-and-morty/static/media/812407.e05c4cf5.jpg"
+  }
+]);
